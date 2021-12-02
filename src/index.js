975204bd18d4f469d81b810f8d87ee35aa2279ff
Vladislav Zaitsev <vladislav.zaytcev84@gmail.com>
2021-12-02 14:27:13 +0200

    10. реализован вывод если меньше 10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import './css/styles.css';
+import{fetchCountries} from './fetchCountries.js';
+import debounce from 'lodash/debounce';
+import Notiflix from 'notiflix';
 
+const inputJS = document.querySelector("#search-box")
 const DEBOUNCE_DELAY = 300;
+
+const inputHandler = (event) => {
+  const trimSt = event.target.value.trim()
+  if(trimSt.length <= 0){
+    return
+  }
+  console.log(trimSt)
+  fetchCountries(trimSt)
+  .then((countries) => countriesLogic(countries))
+  .catch((error) => console.log(error));
+
+}
+
+inputJS.addEventListener("input", debounce(inputHandler, DEBOUNCE_DELAY))
+
+function countriesLogic(countries){
+  if(countries.length > 10){
+    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+  } else if(countries.length <= 10 && countries.length >= 2){
+    console.log("<= 10 && countries.length >= 2)")
+  } else {
+    console.log("=1")
+  }
+}
